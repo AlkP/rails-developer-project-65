64bd5521cac7a280c22ccd8d017876c821d54b11
Alexey <apasenko@gmail.com>
2026-03-17 20:39:47 +0300

    Выгрузка

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,14 +4,15 @@ set -o errexit
 
 bundle install
 
-# Игнорируем защиту Rails, делать единоразово, затем комментить
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:schema:load
+## Игнорируем защиту Rails, делать единоразово, затем комментить
+#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:schema:load
 
 # Принудительное создание таблиц Solid Queue (если их нет)
 bundle exec rails runner 'load(Rails.root.join("db/queue_schema.rb"))'
 
 bundle exec rails db:migrate
 bundle exec rails db:migrate:queue
+#bundle exec rails db:seed
 
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
